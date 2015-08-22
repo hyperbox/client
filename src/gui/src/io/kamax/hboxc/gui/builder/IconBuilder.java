de7065f82aad7c21149b8344f29c8785b184aa9b
Maxime Dor <max@kamax.io>
2015-08-22 18:59:13 +0200

    Added config key for GUI icons base directory

--- src/gui/src/io/kamax/hboxc/gui/builder/IconBuilder.java
+++ src/gui/src/io/kamax/hboxc/gui/builder/IconBuilder.java
@@ -1,19 +1,19 @@
 /*
  * Hyperbox - Virtual Infrastructure Manager
  * Copyright (C) 2013 Maxime Dor
- * 
+ *
  * http://kamax.io/hbox/
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -38,6 +38,9 @@ import java.util.Map;
 import javax.swing.ImageIcon;
 
 public class IconBuilder {
+
+    public static final String CFGKEY_ICON_BASE_DIR = "client.gui.icon.dir";
+    public static final String CFGVAL_ICON_BASE_DIR = "icons/";
 
     private static String basePath;
     private static ImageIcon unknownElement = new ImageIcon(getBasePath() + "help.png");
@@ -76,7 +79,8 @@ public class IconBuilder {
 
     private static String getBasePath() {
         if (basePath == null) {
-            basePath = Configuration.getSetting(HyperboxClient.CFGKEY_BASE_DIR, HyperboxClient.CFGVAL_BASE_DIR) + File.separator + "icons/";
+            basePath = Configuration.getSetting(CFGKEY_ICON_BASE_DIR, Configuration.getSetting(HyperboxClient.CFGKEY_BASE_DIR, HyperboxClient.CFGVAL_BASE_DIR)
+                    + File.separator + CFGVAL_ICON_BASE_DIR);
         }
 
         return basePath;
